c7fbe4ea66f164fd4ab3533ee65e01b3d7943ae2
Rio Lawrence C. Mamalia <riolawrencemamalia@gmail.com>
2025-12-03 21:46:27 +0800

    Add files via upload

---
+++ Teacher.java
@@ -0,0 +1,15 @@
+public class Teacher {
+    String name;
+    int age;
+    String subject;
+
+    public static void main(String[] args) {
+        Teacher teacher = new Teacher();
+        teacher.name = "Philip John Belp";
+        teacher.age = 25;
+        teacher.subject = "Programming";
+    
+    System.out.println("Name: " + teacher.name + ", Age: " + teacher.age + ", Subject: " + teacher.subject);
+
+    }
+}
